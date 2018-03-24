00003839dc69a8df7061510da661eeafa082f36a
vitaminus <partadw@gmail.com>
2018-03-24 12:15:01 +0200

    Add concern commentable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
+  include Commentable
+
   belongs_to :commentable, polymorphic: true
   belongs_to :user
-
-  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 255 }
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,10 @@
 class Message < ApplicationRecord
+  include Commentable
+
   paginates_per 10
 
   belongs_to :user
   belongs_to :wall
-  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :body, presence: true
 end
